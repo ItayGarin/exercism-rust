56cc50826af30907bcdb34a49bb46464a4582e40
thifi <thifixp@gmail.com>
2016-05-17 07:45:53 +0300

    Add the solution for etl

---
+++ etl/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::BTreeMap;
+
+pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+    let mut output = BTreeMap::new();
+
+    for (&n, vec) in input.iter() {
+        for s in vec.iter() {
+            output.insert(s.to_lowercase(), n);
+        }
+    }
+
+    output
+}
